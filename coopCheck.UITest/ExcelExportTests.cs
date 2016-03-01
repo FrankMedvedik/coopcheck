26d00f916557cb62affdda6849c1d9ab3c9c2f53
Frank Medvedik <fmedvedik@gmail.com>
2016-03-01 17:48:32 -0500

    fixed cloning

---
+++ coopCheck.UITest/ExcelExportTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace coopCheck.UITest
+{
+    [TestClass]
+    public class ExcelExportTests
+    {
+        [TestMethod]
+        public void LoadWorkBookTest()
+        {
+            throw new AssertInconclusiveException();
+            
+        }
+        [TestMethod]
+        public void LoadWorkSheetTest()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ImportWorkSheetTest()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void AddWorkSheetTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void AddNonUniqueNamedWorkSheetTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ExportErrorsWorkSheetTest()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
