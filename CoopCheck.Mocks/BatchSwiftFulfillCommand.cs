f90b94a5f6adfabdbe05fec3999c7bf3e9d082f6
Frank Medvedik <fmedvedik@gmail.com>
2016-04-28 15:08:26 -0400

    added mocks (correctly)

---
+++ CoopCheck.Mocks/BatchSwiftFulfillCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoopCheck.Mocks
+{
+    public class BatchSwiftFulfillCommand
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static void BatchSwiftFulfill(int batchNum , bool pass)
+        {
+            log.Info(String.Format("SwiftPay called user {0}  batch {1}", Environment.UserName,batchNum));
+            System.Threading.Thread.Sleep(5000);
+            log.Info(String.Format("SwiftPay completed {0}  batch {1} completed ok {2} ", Environment.UserName, batchNum, pass));
+            if (!pass) throw new Exception("Swiftpay failed");
+            
+        }
+    }
+}
