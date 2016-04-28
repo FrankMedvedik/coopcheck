77d664f95f1ede659bcb9ebc975e35f8cbe6fa61
Frank Medvedik <fmedvedik@gmail.com>
2016-04-28 17:13:29 -0400

    moved coopcheck wpf and csla projects up to same directory level

---
+++ CoopCheck.Web/Services/SendMailSvc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CoopCheck.Web.Services
+{
+    public static class SendMailSvc
+    {
+        private static readonly log4net.ILog log
+         = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void SendEmail(string to, string subject, string body)
+        {
+            try
+            {
+
+                string from = "Fmedvedik@reckner.com";
+                string _ms = "email.reckner.com";
+                MailMessage msg = new MailMessage(from, to, subject, body);
+                SmtpClient sc = new SmtpClient(_ms);
+                sc.Send(msg);
+            }
+            catch (Exception e)
+            {
+                log.Error(String.Format("EMAIL FAILED TO SEND Error: {0}", e.Message));
+            }
+        }
+
+
+    }
+}
---
+++ CoopCheck.Web/Services/SwiftPaySvc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using CoopCheck.Library;
+
+namespace CoopCheck.Web.Services
+{
+    public class SwiftPaySvc
+    {
+        private static readonly log4net.ILog log
+       = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static async Task PayBatch(int accountId, int batchNum, WindowsPrincipal user)
+        {
+            
+            log.Info(String.Format("SwiftPay called user {0}  account {1} batchNum {2}", user.Identity.Name, accountId, batchNum));
+            try
+            {
+                await Task.Factory.StartNew(() =>
+                {
+                    CoopCheck.Mocks.BatchSwiftFulfillCommand.BatchSwiftFulfill(batchNum,true);
+                });
+                SendMailSvc.SendEmail("fmedvedik@reckner.com",
+                    String.Format("Swiftpay complete for batch {0}", batchNum), "processing complete");
+            }
+            catch (Exception e)
+            {
+                log.Error(String.Format("SwiftPay failed user {0}  account {1} batchNum {2} error {3}", user.Identity.Name, accountId, batchNum, e.Message));
+                    SendMailSvc.SendEmail("fmedvedik@reckner.com",
+                    String.Format("Swiftpay Error for batch {0}", batchNum), e.Message);
+            }
+
+        }
+    }
+}
